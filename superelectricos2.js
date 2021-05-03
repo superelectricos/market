8a14ed00227193c23875a9c289fdf051b47a2107
superelectricos <83521538+superelectricos@users.noreply.github.com>
2021-05-02 23:31:32 -0400

    Update superelectricos2.js

--- superelectricos2.js
+++ superelectricos2.js
@@ -4,61 +4,55 @@ window.addEventListener('load', function(){
 });
 
 function getClicksDonate(){
-   let clicks;
    $.ajax({
    url:"https://api.countapi.xyz/get/github.io.superelectricos/donate",
    success: function(data) {
-      clicks = data["value"];   }
+      var clicks = data["value"];   }
    });
    return clicks;
 }
 
 function getClicksArcade(){
-   let clicks;
    $.ajax({
    url:"https://api.countapi.xyz/get/github.io.superelectricos/arcade",
    success: function(data) {
-      clicks = data["value"];   }
+      var clicks = data["value"];   }
    });
    return clicks;
 }
 
 function getClicksDarthVader(){
-   let clicks;
    $.ajax({
    url:"https://api.countapi.xyz/get/github.io.superelectricos/darthvader",
    success: function(data) {
-      clicks = data["value"];   }
+      var clicks = data["value"];   }
    });
    return clicks;
 }
 
 function getClicksColgante(){
-   let clicks;
    $.ajax({
    url:"https://api.countapi.xyz/get/github.io.superelectricos/colgante",
    success: function(data) {
-      clicks = data["value"];   }
+      var clicks = data["value"];   }
    });
    return clicks;
 }
 
 function getClicksReloj(){
-   let clicks;
    $.ajax({
    url:"https://api.countapi.xyz/get/github.io.superelectricos/reloj",
    success: function(data) {
-      clicks = data["value"];   }
+      var clicks = data["value"];   }
    });
    return clicks;
 }
 
 function getClicksLlavero(){
-   let clicks;
    $.ajax({
    url:"https://api.countapi.xyz/get/github.io.superelectricos/llavero",
    success: function(data) {
-      clicks = data["value"];   }
+      var clicks = data["value"];   }
    });
    return clicks;
 }
