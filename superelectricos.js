06f0b37c82589719badd7cf31fdd01935562edf9
superelectricos <83521538+superelectricos@users.noreply.github.com>
2021-05-02 22:47:21 -0400

    Update superelectricos.js

--- superelectricos.js
+++ superelectricos.js
@@ -40,3 +40,87 @@ function getClicksDonate(){
    });
    return clicks;
 }
+
+function addVisitsArcade() {
+  $.ajax({
+   url:"https://api.countapi.xyz/hit/github.io.superelectricos/arcade",
+   type: "GET"
+   });
+}
+
+var arcade = document.getElementById('arcade');
+arcade.addEventListener('click', addVisitsArcade);
+
+function getClicksArcade(){
+   let clicks;
+   $.ajax({
+   url:"https://api.countapi.xyz/get/github.io.superelectricos/arcade",
+   success: function(data) {
+      clicks = data["value"];   }
+   });
+   return clicks;
+}
+
+function addVisitsDarthVader() {
+  $.ajax({
+   url:"https://api.countapi.xyz/hit/github.io.superelectricos/darthvader",
+   type: "GET"
+   });
+}
+
+var darthvader = document.getElementById('darthvader');
+darthvader.addEventListener('click', addVisitsDarthVader);
+
+function getClicksDarthVader(){
+   let clicks;
+   $.ajax({
+   url:"https://api.countapi.xyz/get/github.io.superelectricos/darthvader",
+   success: function(data) {
+      clicks = data["value"];   }
+   });
+   return clicks;
+}
+
+
+function addVisitsColgante() {
+  $.ajax({
+   url:"https://api.countapi.xyz/hit/github.io.superelectricos/colgante",
+   type: "GET"
+   });
+}
+
+var colgante = document.getElementById('colgante');
+colgante.addEventListener('click', addVisitsColgante);
+
+function getClicksColgante(){
+   let clicks;
+   $.ajax({
+   url:"https://api.countapi.xyz/get/github.io.superelectricos/colgante",
+   success: function(data) {
+      clicks = data["value"];   }
+   });
+   return clicks;
+}
+
+function addVisitsReloj() {
+  $.ajax({
+   url:"https://api.countapi.xyz/hit/github.io.superelectricos/reloj",
+   type: "GET"
+   });
+}
+
+var reloj = document.getElementById('reloj');
+reloj.addEventListener('click', addVisitsReloj);
+
+function getClicksReloj(){
+   let clicks;
+   $.ajax({
+   url:"https://api.countapi.xyz/get/github.io.superelectricos/reloj",
+   success: function(data) {
+      clicks = data["value"];   }
+   });
+   return clicks;
+}
+
+
+
